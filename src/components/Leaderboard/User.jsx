28b6488bab87ece7b46ea337c2e511fe867e9613
Samir <aliyevsamir950@gmail.com>
2020-08-15 17:32:36 +0400

    submission view on profile changed to modal view

--- src/components/Leaderboard/User.jsx
+++ src/components/Leaderboard/User.jsx
@@ -8,18 +8,30 @@ import { getSubmission } from '../../redux/actions/quizzes';
 import QuizTemplate2 from '../QuizTamplate2/QuizTemplate2';
 import { useState } from 'react';
 import Loading from '../../utils/Loading';
+import Modal from 'antd/lib/modal/Modal';
+import './LeaderboardList.scss';
 
 const User = ({ user, getSubmission, showSubmission }) => {
     const [content, setContent] = useState({
-        loading: false,
         content: null
     });
 
-    const handleClick = async (quizId, submissionId) => {
+    const [showQuizSubmission, setShowQuizSubmission] = useState(false);
+
+    const handleClose = () => {
+        setShowQuizSubmission(false);
         setContent({
-            loading: true,
             content: null
         });
+        console.log(showQuizSubmission);
+    };
+
+    const handleClick = async (quizId, submissionId) => {
+        setContent({
+            content: <Loading />
+        });
+
+        setShowQuizSubmission(true);
 
         const submission = await getSubmission(quizId, submissionId);
 
@@ -30,67 +42,76 @@ const User = ({ user, getSubmission, showSubmission }) => {
                     questions={submission.submission}
                     type='submission'
                     name={submission.created_by}
-                    from='profile'
                 />
             )
         });
     };
+
+    const mainStyle = {
+        lineHeight: '50px',
+        fontSize: '1.2rem',
+        fontWeight: '600'
+    };
     const additionalProps = showSubmission
         ? {
-              style: { cursor: 'pointer' },
+              style: { cursor: 'pointer', ...mainStyle },
               onClick: () => handleClick(user.quiz_id, user.quiz_submission_id)
           }
-        : {};
+        : { style: { ...mainStyle } };
 
     return (
-        <Row
-            type='flex'
-            justify='center'
-            align='middle'
-            {...additionalProps}
-            style={{ lineHeight: '50px' }}
-        >
-            <Col
-                span={4}
-                style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    color: '#000'
-                }}
+        <>
+            <Row
+                type='flex'
+                justify='center'
+                align='middle'
+                {...additionalProps}
             >
-                {user.rank == 1 ? (
-                    <img src={gold} style={{ height: '50px' }} />
-                ) : user.rank == 2 ? (
-                    <img src={silver} style={{ height: '50px' }} />
-                ) : user.rank == 3 ? (
-                    <img src={bronze} style={{ height: '50px' }} />
-                ) : (
-                    user.rank
-                )}
-            </Col>
-            <Col
-                span={16}
-                style={{
-                    textAlign: 'start',
-                    lineHeight: '1'
-                }}
-            >
-                {user.name}
-            </Col>
-            <Col span={4} style={{ color: 'tomato' }}>
-                {user.points}
-            </Col>
-            {!content.loading && content.content ? (
-                <Col span={24} style={{}}>
-                    {content.content}
+                <Col
+                    span={4}
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        color: '#000'
+                    }}
+                >
+                    {user.rank == 1 ? (
+                        <img src={gold} style={{ height: '50px' }} />
+                    ) : user.rank == 2 ? (
+                        <img src={silver} style={{ height: '50px' }} />
+                    ) : user.rank == 3 ? (
+                        <img src={bronze} style={{ height: '50px' }} />
+                    ) : (
+                        user.rank
+                    )}
                 </Col>
-            ) : content.loading && !content.content ? (
-                <Col span={24}>
-                    <Loading />
+                <Col
+                    span={16}
+                    style={{
+                        textAlign: 'start',
+                        lineHeight: '1'
+                    }}
+                >
+                    {user.name}
                 </Col>
+                <Col span={4} style={{ color: 'tomato' }}>
+                    {user.points}
+                </Col>
+            </Row>
+
+            {showQuizSubmission ? (
+                <Modal
+                    visible={showQuizSubmission}
+                    onCancel={handleClose}
+                    onOk={handleClose}
+                    cancelButtonProps={{ danger: true }}
+                    cancelText='Çıx'
+                >
+                    <Col span={24}>{content.content}</Col>
+                </Modal>
             ) : null}
-        </Row>
+        </>
     );
 };
 
